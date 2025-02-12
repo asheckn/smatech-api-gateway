5e17b77a491e76670cfe7de572e9123b18cc99b2
Nyasha Asheck Hove <50446026+asheckn@users.noreply.github.com>
2025-02-12 15:24:09 +0200

    Initial commit

---
+++ src/main/java/com/asheck/smatech_api_gateway/SmatechApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.asheck.smatech_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmatechApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmatechApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asheck/smatech_api_gateway/SmatechApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asheck.smatech_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmatechApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
